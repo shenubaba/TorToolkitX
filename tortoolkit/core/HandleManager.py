14c54e4e1881bba8bcf7290425033aa563f568a9
shenubaba <98071639+shenubaba@users.noreply.github.com>
2022-01-22 22:39:58 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -644,7 +644,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/KangersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "🔥Yo..Wuzzup❗️Bro🤟 Are You Want Use Me ❓Contact My Owner👉 @Wuzzup_BoY"
     await event.reply(msg, parse_mode="html")
 
 
